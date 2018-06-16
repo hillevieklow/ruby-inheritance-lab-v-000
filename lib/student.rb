9f58884dcdcf7f70a6c3af178e0047b5da67e036
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-16 14:31:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,6 @@ class Student  < User
 
   KNOWLEDGE = []
 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
-  end
+  attr_accessor :first_name, :last_name
 
 end
